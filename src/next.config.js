5c8e4e4122b1d05907e257b5b650d7163042f744
Matheus Brasil <matheus.brasil10@gmail.com>
2017-07-19 12:50:15 -0300

    chore(next): setup minifying, preact aliasing, service worker and others plugins

---
+++ src/next.config.js
@@ -0,0 +1,45 @@
+const BabiliPlugin = require('babili-webpack-plugin');
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+
+module.exports = {
+	webpack(config, { dev }) {
+
+		if (dev) {
+			return config;
+		}
+
+		config.plugins.push(
+			new SWPrecacheWebpackPlugin({
+				filename: 'sw.js',
+				minify: true,
+				verbose: true,
+				staticFileGlobsIgnorePatterns: [/\.next\//],
+				forceDelete: true,
+				runtimeCaching: [
+					{
+						handler: 'fastest',
+						urlPattern: /[.](png|jpg|css)/
+					},
+					{
+						handler: "networkFirst",
+						urlPattern: /^https.*/
+					}
+				]
+			})
+		);
+
+		config.plugins.push(new BabiliPlugin());
+
+		config.resolve.alias = {
+			"react": "preact-compat/dist/preact-compat",
+			"react-dom": "preact-compat/dist/preact-compat"
+		};
+
+		config.plugins = config.plugins.filter(plugin => {
+			return plugin.constructor.name !== 'UglifyJsPlugin';
+		});
+
+		return config;
+	},
+	distDir: './../build'
+};
