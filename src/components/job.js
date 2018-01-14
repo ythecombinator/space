dceca3c157f2ef85f49626f9c72dcaa8261439eb
Matheus Brasil <matheus.brasil10@gmail.com>
2018-01-14 07:39:15 -0200

    data(general): update some information :smiley:

--- src/components/job.js
+++ src/components/job.js
@@ -3,9 +3,13 @@ import { getRandomEmoji } from '../helpers/emojis';
 
 const Job = ({job, activity}) => (
 	<p>
-		{ `${getRandomEmoji('job')} I'm currently working as an ${job.role} at a ${job.description} called ${job.company.name}–if you want even more accurate data on my programming activity, check`}
+		{ `${getRandomEmoji('job')} I'm currently working as an ${job.role} at a ${job.description} called `}
+		<a href={job.company.link} target="_blank" rel="noopener">
+			{`${job.company.name}`}
+		</a>
+		{`–if you want even more accurate data on my programming activity, check `}
 		<a href={activity.link} target="_blank" rel="noopener">
-			{` here.`}
+			here.
 		</a>
 	</p>
 );
