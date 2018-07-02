6c081d20ef699977e748f70e0fdd1a7e99bf435d
Matheus Brasil <matheus.brasil10@gmail.com>
2018-07-02 08:32:45 -0300

    feat(pages): render markdown from JS strings

---
+++ src/pages/about.data.js
@@ -0,0 +1,72 @@
+export const data = `
+## Intro
+
+✋ Hi folks! I'm just another constantly evolving full-stack developer draft who
+inhabits this world.
+
+💜 My areas of interest/passions include: JavaScript, Swift, architecture 
+patterns, User Interfaces libraries and frameworks developer tooling, 
+functional and reactive programming, unix philosophy, and User Interaction 
+design.
+
+👥 I also try to help as much as I can Brazilian local communities like Dev I/O 
+Foundation, Ionic Brazil, CocoaHeads Fortaleza, Lambda I/O Foundation, and many
+others!
+
+📢 In addition to attending and organizing meetups and conferences, I really like
+to bring cool things to where I'm invited to speak; if you want to see some of 
+my lectures, [here](/talks) they are.
+
+💻 As I mentioned, I also love open source, if you'd like to stalk me on GitHub, 
+[don't ever hesitate](https://github.com/ythecombinator).
+
+## Experience
+
+### Software Engineer
+
+🏢 Beakyn Company
+
+🌎 New York City, USA (Remote)
+
+📅 Sep 2017 – Present
+
+📃 Working to grow the business and, mostly, help our clients meeting their 
+business goals, without leaving quality aside in large-scale geographic data
+visualization platforms and assets curation systems.
+
+👨‍💻 Mostly working with:
+
+- React and Next.js
+- React Native and iOS native development
+- redux and related tooling (e.g. redux-saga, redux-act, reselect etc.)
+- JavaScript and TypeScript
+- Webpack and bundle optimization techniques (prefetching, code splitting, common chunks, dynamic imports, service workers, web workers, compile-time optimization etc.)
+- Map data plotting and analyzation tools (e.g. Mapbox GL, Turf.js, pbf etc.)
+- Firebase services (Auth, Cloud Functions, Firestore etc.)
+- Authentication, authorization and accounting infrastructure (mostly with Auth0 and node.js)
+- Chat & Chatbots tooling (Intercom, Tokbox, Dialogflow etc.)
+
+### Mobile Developer
+
+🏢 Apple Developer Academy
+
+🌎 Fortaleza, Brazil
+
+📅 Jan 2016 – Sep 2017
+
+📃 I've been a macOS, iOS, watchOS, tvOS development intern with a Full-Stack 
+development and User Interaction design background in the Apple Developer 
+Academy – former _Brazilian Education Program for iOS Developers_ (BEPiD) – 
+which aims to grow iOS developers by training Swift/Objective-C and other 
+technologies.
+
+👨‍💻 Mostly working with:
+
+- Swift/Objective-C
+- iOS frameworks and APIs (e.g. Foundation, UIKit, AutoLayout, CoreData, MapKit, CoreLocation, CoreMotion, Push Notifications etc.)
+- watchOS frameworks and APIs (e.g. WatchKit, WatchConnectivity etc.)
+- tvOS frameworks and APIs (e.g. UIKit, Focus Engine, TVMLKit etc.)
+- Server-side Swift frameworks and APIs (e.g. Vapor)
+- Unit and integration tests tools (e.g. XCTest, Quick, Nimble etc.)
+- SpriteKit and other game development focused APIs (e.g. GameCenter)
+`
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import {Markdown} from 'react-showdown';
+import {data} from './about.data';
 
 const StyledAbout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 3rem;
+  h2 {
+    color: #958bfb;
+    font-family: 'Lato', sans-serif;
+  }  
+  h3 {
+    color: #8bb9fb;
+    font-family: 'Lato', sans-serif;
+  }
   p {
     margin: 0;
     color: #333333;
@@ -21,18 +31,28 @@ const StyledAbout = styled.div`
       text-decoration: none;
     }
   }
+  ul {
+    font-family: 'Lato', sans-serif;
+    margin-top: 0.5rem;
+    li {
+      color: #333333;
+      font-family: 'Lato', sans-serif;
+      font-weight: 400;
+      font-size: 1rem;
+      line-height: 2rem;
+      @media (min-width: 768px) {
+        font-size: 1.1rem;
+      }
+      a {
+        color: #fb958b;
+        text-decoration: none;
+      }
+    }
+  }
 `
 
 export default () => (
   <StyledAbout>
-    <p>
-      I'm a client-side architect–with an iOS/front-end and node.js background–currently working as a front-end engineer at Beakyn, a company whose mission is to help people better understand locations by building the most comprehensive geofence and Point of Interest (PoI) data in the America.
-    </p>
-    <p>
-      My areas of interest/passions include: JavaScript, Swift, architecture patterns (redux et al.), User Interfaces libraries and frameworks (React et al.), developer tooling, functional and reactive programming, unix philosophy, and User Interaction design.
-    </p>
-    <p>
-      I also try to help as much as I can Brazilian local communities like Dev I/O Foundation, Ionic Brazil, CocoaHeads Fortaleza, Lambda I/O Foundation, and many others!
-    </p>
+    <Markdown markup={data} />
   </StyledAbout>
 )
