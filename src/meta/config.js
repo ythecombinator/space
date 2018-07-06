4ac19acd8c929c5c9cab3c6489b604a7d7006236
Matheus Brasil <matheus.brasil10@gmail.com>
2018-07-06 01:39:28 -0300

    feat(app): add manifest and service worker

---
+++ src/meta/config.js
@@ -0,0 +1,24 @@
+// const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "ythecombinator",
+  shortSiteTitle: "Full-stack engineer",
+  siteDescription: "Full-stack engineer focused on client-side architecture",
+  siteUrl: "https://ythecombinator.space",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "Матвій Альбукерке",
+  authorTwitterAccount: "ythecombinator",
+  // info
+  infoTitle: "Матвій Альбукерке",
+  infoTitleNote: "It's me, Matheus.",
+  // manifest.json
+  manifestName: "ythecombinator | Full-Stack Engineer",
+  manifestShortName: "Матвій",
+  manifestStartUrl: "/",
+  // manifestBackgroundColor: colors.background,
+  // manifestThemeColor: colors.background,
+  manifestDisplay: "standalone",
+};
