c836e8c907bb99636cc8f8f6fc55ac8fd9a352e7
Matheus Albuquerque <land@ythecombinator.space>
2019-08-19 22:59:17 +0200

    feat(components/pages/talks): shuffle talks

--- src/components/pages/talks/index.tsx
+++ src/components/pages/talks/index.tsx
@@ -6,20 +6,25 @@ import PostList from 'components/PostList';
 
 import {PageProps} from 'model/PageProps';
 
+import {shuffleItems} from 'utils/array';
+
 import {featuredTalks} from 'data/about';
 
 import {StyledCollection, StyledContainer, StyledContent, StyledItem, StyledLink} from './styles';
 
 interface Props extends PageProps {}
 
+const talks = shuffleItems(featuredTalks).slice(0, 9);
+
 const PageWrapper: FunctionComponent<Props> = props => {
   const { data, location } = props;
   const posts = data.allMarkdownRemark.edges;
+
   return (
     <Layout location={location}>
       <StyledContainer>
         <StyledCollection>
-          {featuredTalks.map(talk => (
+          {talks.map(talk => (
             <StyledItem image={talk.image}>
               <StyledContent>
                 <StyledLink to={talk.link}>{talk.event}</StyledLink>
