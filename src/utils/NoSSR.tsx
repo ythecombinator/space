dbaf5c1ea2280e887716c1bb72f6fff31bb0c2b8
Matheus Albuquerque <land@ythecombinator.space>
2019-12-03 18:41:55 +0100

    feat(utils): add NoSSR component

---
+++ src/utils/NoSSR.tsx
@@ -0,0 +1,27 @@
+import React, {FunctionComponent, useEffect, useState} from 'react';
+
+interface Props {
+  defer?: boolean;
+  fallback?: JSX.Element;
+}
+
+const NoSSR: FunctionComponent<Props> = props => {
+  const { children, defer = false, fallback = null } = props;
+  const [mountedState, setMountedState] = useState(false);
+
+  useEffect(() => {
+    if (!defer) {
+      setMountedState(true);
+    }
+  }, [defer]);
+
+  useEffect(() => {
+    if (defer) {
+      setMountedState(true);
+    }
+  }, [defer]);
+
+  return <>{mountedState ? children : fallback}</>;
+};
+
+export default NoSSR;
