15e7cc620ec5cac54fe2747c02f8ef9302e45470
Matheus Albuquerque <land@ythecombinator.space>
2022-11-06 21:42:34 +0100

    content(src/components/templates/talks): add/reorganize pics

--- src/components/templates/talks/utils.ts
+++ src/components/templates/talks/utils.ts
@@ -1,134 +1,156 @@
 import {shuffleItems} from 'utils/array';
 
-import featuredTalk01 from './assets/cejs-2016.jpg';
-import featuredTalk02 from './assets/cejs-2017.jpg';
-import featuredTalk04 from './assets/front_in_fortaleza-2018.jpg';
-import featuredTalk13 from './assets/frontend_talks_brn-2019.jpg';
-import featuredTalk15 from './assets/frontend_talks_brn-2020.jpg';
-import featuredTalk14 from './assets/frontend_talks_prg-2019.jpg';
-import featuredTalk16 from './assets/frontend_talks_prg-2020.jpg';
-import featuredTalk06 from './assets/natal_tech_conference-2019.jpg';
-import featuredTalk03 from './assets/phpeste-2017.jpg';
-import featuredTalk18 from './assets/react_next-2022.jpg';
-import featuredTalk05 from './assets/react_nyc-2018.jpg';
-import featuredTalk17 from './assets/react_summit_2022.jpg';
-import featuredTalk08 from './assets/tdc_bh-2019-1.jpg';
-import featuredTalk09 from './assets/tdc_bh-2019-2.jpg';
-import featuredTalk07 from './assets/tdc_florianopolis-2019.jpg';
-import featuredTalk10 from './assets/tdc_sp-2019-1.jpg';
-import featuredTalk11 from './assets/tdc_sp-2019-2.jpg';
-import featuredTalk12 from './assets/the_conf.jpg';
+import featuredTalkA from './assets/2017/cejs.jpg';
+import featuredTalkB from './assets/2017/phpeste.jpg';
+import featuredTalkC from './assets/2018/front-in-fortaleza.jpg';
+import featuredTalkD from './assets/2018/react-nyc.jpg';
+import featuredTalkE from './assets/2019/frontend-talks-brn.jpg';
+import featuredTalkF from './assets/2019/frontend-talks-prg.jpg';
+import featuredTalkG from './assets/2019/tdc-bh-1.jpg';
+import featuredTalkH from './assets/2019/tdc-bh-2.jpg';
+import featuredTalkI from './assets/2019/tdc-florianopolis.jpg';
+import featuredTalkJ from './assets/2019/tdc-sp-1.jpg';
+import featuredTalkK from './assets/2019/tdc-sp-2.jpg';
+import featuredTalkL from './assets/2019/the-conf.jpg';
+import featuredTalkM from './assets/2020/frontend-talks-brn.jpg';
+import featuredTalkN from './assets/2020/frontend-talks-prg.jpg';
+import featuredTalkO from './assets/2022/devconf-pl.jpg';
+import featuredTalkP from './assets/2022/react-advanced.jpg';
+import featuredTalkQ from './assets/2022/react-alicante.jpg';
+import featuredTalkR from './assets/2022/react-brussels.jpg';
+import featuredTalkS from './assets/2022/react-india.jpg';
+import featuredTalkT from './assets/2022/react-next.jpg';
+import featuredTalkU from './assets/2022/react-summit.jpg';
 
 const featuredTalks = [
   {
-    event: "React Next 2022 • June, 2022",
-    link: "/talks/inside-fiber-the-in-depth-overview-you-wanted-a-tldr-for",
-    image: featuredTalk18,
-    key: "featuredTalk18",
+    event: "React Advanced London • October, 2022",
+    link: "/talks/deep-diving-on-concurrent-react",
+    image: featuredTalkP,
+    key: "featuredTalkP",
   },
   {
-    event: "React Summit 2022 • June, 2022",
+    event: "React Brussels • October, 2022",
+    link: "/talks/deep-diving-on-concurrent-react",
+    image: featuredTalkR,
+    key: "featuredTalkR",
+  },
+  {
+    event: "React Alicante • October, 2022",
     link: "/talks/inside-fiber-the-in-depth-overview-you-wanted-a-tldr-for",
-    image: featuredTalk17,
-    key: "featuredTalk17",
+    image: featuredTalkQ,
+    key: "featuredTalkQ",
+  },
+  {
+    event: "React India • September, 2022",
+    link: "/talks/deep-diving-on-concurrent-react",
+    image: featuredTalkS,
+    key: "featuredTalkS",
+  },
+  {
+    event: "DevConf PL • September, 2022",
+    link:
+      "/talks/consistent-ux-at-scale-lessons-learned-when-i-wore-the-design-ops-hat",
+    image: featuredTalkO,
+    key: "featuredTalkO",
+  },
+  {
+    event: "React Next • June, 2022",
+    link: "/talks/inside-fiber-the-in-depth-overview-you-wanted-a-tldr-for",
+    image: featuredTalkT,
+    key: "featuredTalkT",
+  },
+  {
+    event: "React Summit • June, 2022",
+    link: "/talks/inside-fiber-the-in-depth-overview-you-wanted-a-tldr-for",
+    image: featuredTalkU,
+    key: "featuredTalkU",
   },
   {
     event: "Frontend Talks PRG • February, 2020",
     link: "/talks/the-best-is-yet-to-come-the-future-of-react",
-    image: featuredTalk16,
-    key: "featuredTalk16",
+    image: featuredTalkN,
+    key: "featuredTalkN",
   },
   {
     event: "Frontend Talks BRN • February, 2020",
     link: "/talks/the-best-is-yet-to-come-the-future-of-react",
-    image: featuredTalk15,
-    key: "featuredTalk15",
+    image: featuredTalkM,
+    key: "featuredTalkM",
   },
   {
     event: "Frontend Talks PRG • November, 2019",
     link:
       "/talks/code-sharing-at-scale-one-codebase-for-web-mobile-and-desktop",
-    image: featuredTalk14,
-    key: "featuredTalk14",
+    image: featuredTalkF,
+    key: "featuredTalkF",
   },
   {
     event: "Frontend Talks BRN • November, 2019",
     link:
       "/talks/code-sharing-at-scale-one-codebase-for-web-mobile-and-desktop",
-    image: featuredTalk13,
-    key: "featuredTalk13",
+    image: featuredTalkE,
+    key: "featuredTalkE",
   },
   {
     event: "The Conf • 2019",
     link: "/talks/the-day-i-reverse-engineered-a-gameboy-advance-game",
-    image: featuredTalk12,
-    key: "featuredTalk12",
+    image: featuredTalkL,
+    key: "featuredTalkL",
   },
   {
     event: "TDC 2019 • São Paulo",
     link: "/talks/react-16-dot-x-way-beyond-hooks",
-    image: featuredTalk11,
-    key: "featuredTalk11",
+    image: featuredTalkJ,
+    key: "featuredTalkJ",
   },
   {
     event: "TDC 2019 • São Paulo",
     link: "/talks/react-16-dot-x-way-beyond-hooks",
-    image: featuredTalk10,
-    key: "featuredTalk10",
+    image: featuredTalkK,
+    key: "featuredTalkK",
   },
   {
     event: "TDC 2019 • Belo Horizonte",
     link: "/talks/the-state-of-the-state-react-state-management-in-2019",
-    image: featuredTalk09,
-    key: "featuredTalk09",
+    image: featuredTalkG,
+    key: "featuredTalkG",
   },
   {
     event: "TDC 2019 • Belo Horizonte",
     link: "/talks/react-16-dot-x-way-beyond-hooks",
-    image: featuredTalk08,
-    key: "featuredTalk08",
+    image: featuredTalkH,
+    key: "featuredTalkH",
   },
   {
     event: "TDC 2019 • Florianópolis",
     link: "/talks/the-state-of-the-state-react-state-management-in-2019",
-    image: featuredTalk07,
-    key: "featuredTalk07",
-  },
-  {
-    event: "Natal Tech Conference • 2019",
-    link: "/talks/the-hitchhikers-guide-to-the-front-end-performance",
-    image: featuredTalk06,
-    key: "featuredTalk06",
+    image: featuredTalkI,
+    key: "featuredTalkI",
   },
   {
     event: "React NYC • October, 2018",
     link: "/talks/state-of-the-art-react-development-flow-with-typescript",
-    image: featuredTalk05,
-    key: "featuredTalk05",
+    image: featuredTalkD,
+    key: "featuredTalkD",
   },
   {
     event: "Front In Fortaleza • 2018",
     link: "/talks/the-hitchhikers-guide-to-the-front-end-performance",
-    image: featuredTalk04,
-    key: "featuredTalk04",
+    image: featuredTalkC,
+    key: "featuredTalkC",
   },
   {
     event: "PHPeste • 2017",
     link: "/talks/the-correctness-by-design-duck-will-bite-php",
-    image: featuredTalk03,
-    key: "featuredTalk03",
+    image: featuredTalkB,
+    key: "featuredTalkB",
   },
   {
     event: "CEJS • 2017",
     link: "/talks/sweet-macros-o-mine",
-    image: featuredTalk02,
-    key: "featuredTalk02",
-  },
-  {
-    event: "CEJS • 2016",
-    link: "/talks/javascript-in-the-sky-with-types",
-    image: featuredTalk01,
-    key: "featuredTalk01",
+    image: featuredTalkA,
+    key: "featuredTalkA",
   },
 ];
 
