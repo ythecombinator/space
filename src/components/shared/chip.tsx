9f861977b0a6d197a85dce203ffaec5b101aa95a
Matheus Albuquerque <land@ythecombinator.space>
2023-06-24 23:02:49 +0200

    🐛 fix(components/shared/chip): add missing dark mode

--- src/components/shared/chip.tsx
+++ src/components/shared/chip.tsx
@@ -6,11 +6,9 @@ import { FunctionComponent, PropsWithChildren } from 'react';
 
 const Chip: FunctionComponent<PropsWithChildren<{}>> = ({ children }) => {
   return (
-    <span className="no-underline relative inline-block text-sm font-medium text-blue-700 group mr-4">
-      <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-blue-700 group-hover:translate-y-0 group-hover:translate-x-0"></span>
-      <em className="relative block px-2 py-1 bg-white border border-current">
-        {children}
-      </em>
+    <span className="no-underline relative inline-block text-sm font-medium text-blue-700 dark:text-blue-300 group mr-4">
+      <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-blue-700 dark:bg-blue-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+      <em className="relative block px-2 py-1 bg-white dark:bg-gray-800 border border-current">{children}</em>
     </span>
   );
 };
