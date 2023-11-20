75327c258cec08db0ab046b947b1dfc244f62601
Matheus Albuquerque <land@ythecombinator.space>
2023-11-20 14:57:11 +0100

    ✨ feat(services): query CFPs

---
+++ src/services/talks-cfp-content-service.ts
@@ -0,0 +1,97 @@
+import { pathOr } from 'ramda';
+
+import NotionService, { NotionResponse } from 'services/notion-service';
+
+import { formatDate, isSingleDayTimeSpan } from 'utils/date';
+
+const notionServiceInstance = NotionService.getInstance();
+
+export default class TalksCFPContentService {
+  private static instance: TalksCFPContentService;
+  private seasonMap: Record<ConferenceSeason, string> = {
+    '2024': '57cbd3d5d34c4933bc51a7a49737f4f8',
+    '2023': '6f8ad796bb5746a1a479446a7153d470',
+    '2022': '2dee03c01edf473eb35635ea9f1edcde',
+  };
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new TalksCFPContentService();
+    }
+
+    return this.instance;
+  }
+
+  public async get(season: ConferenceSeason) {
+    const result = await notionServiceInstance.queryDataBase(this.seasonMap[season]);
+    return result.map(notionResponseTransformer);
+  }
+
+  public async getAll() {
+    const results = await Promise.all(
+      Object.keys(this.seasonMap).map(async (season) => {
+        return { season, data: await this.get(season as ConferenceSeason) };
+      })
+    );
+
+    return results.reduce((acc, { season, data }) => ({ ...acc, [season]: data }), {}) as Record<
+      ConferenceSeason,
+      EventEntry[]
+    >;
+  }
+}
+
+/*~
+ * TRANSFORMERS (MAIN)
+ */
+
+const notionResponseTransformer = (response: NotionResponse) => {
+  const { event, country, city, dates, deadline, result } = response.properties;
+
+  return {
+    event: event.title.map((chunk) => chunk.plain_text).join(' '),
+    country: country?.select.name,
+    city: city.rich_text.map((chunk) => chunk.plain_text).join(''),
+    eventWebsite: pathOr('N/A', ['properties', 'eventWebsite', 'rich_text', 0, 'text', 'content'], result),
+    dates: {
+      isSingleDayEvent: isSingleDayTimeSpan(dates.date.start, dates.date.end),
+      start: {
+        raw: dates.date.start,
+        formatted: formatDate(dates.date.start),
+      },
+      end: {
+        raw: dates.date.end,
+        formatted: formatDate(dates.date.end),
+      },
+    },
+    deadline: deadline?.date?.start
+      ? {
+          raw: deadline.date.start,
+          formatted: formatDate(deadline.date.start),
+        }
+      : { raw: 'N/A', formatted: 'N/A' },
+    cfpWebsite: pathOr('N/A', ['properties', 'cfpWebsite', 'rich_text', 0, 'text', 'content'], result),
+    result: result.select.name as EngagementStatusPrimary,
+    statusSecondary: response.properties['result-followup']?.select.name as EngagementStatusSecondary,
+  };
+};
+
+/*~
+ * TYPES
+ */
+
+export type ConferenceSeason = '2022' | '2023' | '2024';
+
+export type EngagementStatusPrimary =
+  | 'CANCELED'
+  | 'NO FEEDBACK'
+  | 'WAITING'
+  | 'REJECTED'
+  | 'TO SUBMIT'
+  | 'NOT SUBMITTED'
+  | 'SELECTED'
+  | 'INVITED';
+
+export type EngagementStatusSecondary = 'N/A' | 'PRESENTED' | 'TO BE PRESENTED' | 'TO BE CONFIRMED' | 'REJECTED';
+
+export type EventEntry = ReturnType<typeof notionResponseTransformer>;
