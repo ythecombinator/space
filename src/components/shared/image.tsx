90738ad04ce92940b5ff9e4a87151af45376d400
Matheus Albuquerque <land@ythecombinator.space>
2023-12-05 18:59:54 +0100

    feat(components/shared): new `Image` primitive

---
+++ src/components/shared/image.tsx
@@ -0,0 +1,46 @@
+import NextImage from 'next/image';
+import React, { ComponentPropsWithoutRef, useState } from 'react';
+
+import { classNames } from 'utils/styles';
+
+/*~
+ * TYPES
+ */
+
+export type ImageProps = {
+  containerClassName?: string;
+  imageClassName?: string;
+  lazy?: boolean;
+} & ComponentPropsWithoutRef<typeof NextImage>;
+
+/*~
+ * COMPONENT
+ */
+
+const Image = (props: ImageProps) => {
+  const { alt, src, containerClassName, imageClassName, lazy = true, ...rest } = props;
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <div
+      className={classNames('overflow-hidden', isLoading && 'animate-pulse', containerClassName)}
+      data-testid="image-container"
+    >
+      <NextImage
+        xwx={classNames(
+          'transition-[scale,filter] duration-700',
+          isLoading && 'scale-[1.02] blur-xl grayscale',
+          imageClassName
+        )}
+        src={src}
+        alt={alt}
+        loading={lazy ? 'lazy' : undefined}
+        priority={!lazy}
+        quality={100}
+        onLoad={() => setLoading(false)}
+        {...rest}
+      />
+    </div>
+  );
+};
+export default Image;
