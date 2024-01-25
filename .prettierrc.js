2e5922cc899b5809303d5aea21e9b2b35bffb0b5
Matheus Albuquerque <land@ythecombinator.space>
2024-01-25 12:15:35 +0100

    chore(prettierrc): `proseWrap` and `printWidth: 80` for md

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,22 +1,31 @@
 module.exports = {
-  "printWidth": 120,
-  "semi": true,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "arrowParens": "always",
-  "importOrder": [
-    "<THIRD_PARTY_MODULES>",
-    "^config/(.*)$",
-    "^services/(.*)$",
-    "^schema/(.*)$",
-    "^utils/(.*)$",
-    "^content/(.*)$",
-    "^components/shared/(.*)$",
-    "^components/layouts/(.*)$",
-    "^components/pages/(.*)$",
-    "^styles/(.*)$",
-    "^[./]"
+  printWidth: 120,
+  proseWrap: 'always',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^config/(.*)$',
+    '^services/(.*)$',
+    '^schema/(.*)$',
+    '^utils/(.*)$',
+    '^content/(.*)$',
+    '^components/shared/(.*)$',
+    '^components/layouts/(.*)$',
+    '^components/pages/(.*)$',
+    '^styles/(.*)$',
+    '^[./]',
   ],
-  "importOrderSeparation": true,
-  "plugins": ["@trivago/prettier-plugin-sort-imports"]
-}
+  importOrderSeparation: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  overrides: [
+    {
+      files: '**/*.{md,mdx}',
+      options: {
+        printWidth: 80,
+      },
+    },
+  ],
+};
