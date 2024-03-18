08d7ba88a9006f79d5e95982cec88ef3eeecad26
Matheus Albuquerque <land@ythecombinator.space>
2024-03-18 09:32:20 +0100

    💄 feat(talks/upcoming-talks-section): render `eventDate`

--- src/components/pages/talks/upcoming-talks-section.tsx
+++ src/components/pages/talks/upcoming-talks-section.tsx
@@ -1,8 +1,10 @@
 import { FunctionComponent, PropsWithChildren } from 'react';
-import { FaArrowRight } from 'react-icons/fa';
 
 import { UpcomingSession } from 'services/content/talks';
 
+import { formatDate } from 'utils/date';
+
+import Link from 'components/shared/link';
 import SectionContainer from 'components/shared/section-container';
 import SectionHeading from 'components/shared/section-heading';
 import Typography from 'components/shared/typography';
@@ -24,24 +26,19 @@ const UpcomingTalksSectionItem: FunctionComponent<UpcomingSession> = ({
   talkSlug,
   eventLocation,
   eventName,
+  eventDate,
 }) => {
   return (
-    <div className="group">
-      <a
-        href={talkSlug}
-        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
-      >
-        <div className="flex flex-col">
-          <Typography.small className="text-neutral-900 dark:text-neutral-100 my-0">
-            {eventLocation} • {eventName}
-          </Typography.small>
-          <Typography.h3 className="mt-2">{talkTitle}</Typography.h3>
-        </div>
-        <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
-          <FaArrowRight />
-        </div>
-      </a>
-    </div>
+    <Link
+      href={talkSlug}
+      className="flex w-full flex-col justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
+    >
+      <Typography.subtle className="text-neutral-900 dark:text-neutral-100">
+        {eventLocation} • {<time dateTime={eventDate.toString()}>{formatDate(eventDate)}</time>}
+      </Typography.subtle>
+      <Typography.h3 className="mt-2">{talkTitle}</Typography.h3>
+      <Typography.small className="mt-2">{eventName}</Typography.small>
+    </Link>
   );
 };
 
@@ -49,7 +46,7 @@ const UpcomingTalksSection: FunctionComponent<PropsWithChildren<UpcomingTalksSec
   return (
     <SectionContainer>
       <SectionHeading title="️Upcoming Sessions" />
-      <div className="my-8 flex flex-col space-y-4 w-full">
+      <div className="my-8 flex w-full flex-col space-y-4">
         {items.map((item) => (
           <UpcomingTalksSectionItem key={`${item.talkSlug}-${item.eventName}`} {...item} />
         ))}
