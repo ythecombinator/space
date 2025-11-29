717ee71069541c73557b536a3b34cf512976bf4e
Matheus Albuquerque <land@ythecombinator.space>
2025-11-29 17:44:39 -0300

    feat(empty-list): improved spacing

--- src/components/shared/empty-list.tsx
+++ src/components/shared/empty-list.tsx
@@ -18,7 +18,7 @@ interface EmptyListProps {
 export const EmptyList: FunctionComponent<PropsWithChildren<EmptyListProps>> = ({ heading, subHeading }) => {
   return (
     <div className="grid w-full place-items-center rounded-xl border border-dashed border-gray-400 p-8 text-center">
-      <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">{heading}</h1>
+      <h1 className="text-2xl font-semibold text-gray-900 mt-2 dark:text-gray-100">{heading}</h1>
       <Typography.p>{subHeading}</Typography.p>
     </div>
   );
