54f21be9e02e3573bed2fe3a3b4f7aa739e8da78
Matheus Albuquerque <land@ythecombinator.space>
2025-12-22 05:06:33 -0300

    feat(utils/date.ts): handle nullable inputs

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,12 @@
-export const formatDate = (dateString: string | number) =>
-  new Intl.DateTimeFormat('en-US', {
+export function formatDate(dateString: string | number | null | undefined): string {
+  if (!dateString) return '';
+
+  return new Intl.DateTimeFormat('en-US', {
     month: 'short',
     day: 'numeric',
     year: 'numeric',
   }).format(new Date(dateString));
+}
 
 export const isSingleDayTimeSpan = (startingDate: string | number, endingDate: string | number) => {
   return formatDate(startingDate) === formatDate(endingDate);
